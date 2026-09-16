031ababae0339d097412c8578399e4ca1fccca41
YehorMartynenko <yehor.martynenko.27@gmail.com>
2026-09-16 19:05:18 +0200

    Feat: add print/save pdf button

--- src/components/CvPreview.jsx
+++ src/components/CvPreview.jsx
@@ -18,61 +18,69 @@ function normalizeUrl(url) {
 
 export default function CvPreview({ generalInfo }) {
   return (
-    <div className="preview-container">
-      <h1 className="cv-name">{generalInfo.name || "Your Name"}</h1>
-      <div className="cv-contact-info">
-        {generalInfo.email && (
-          <div className="info-item">
-            <img
-              className="icon"
-              src={emailIcon}
-            />
-            <span className="contact-item">{generalInfo.email}</span>
-          </div>
-        )}
-        {generalInfo.phone && (
-          <div className="info-item">
-            <img
-              className="icon"
-              src={phoneIcon}
-            />
-            <span className="contact-item">{generalInfo.phone}</span>
-          </div>
-        )}
-        {generalInfo.linkedin && (
-          <div className="info-item">
-            <img
-              className="icon"
-              src={linkedinIcon}
-            />
-            <a
-              href={normalizeUrl(generalInfo.linkedin)}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="contact-item"
-            >
-              {getUsername(generalInfo.linkedin)}
-            </a>
-          </div>
-        )}
+    <>
+      <div className="preview-container">
+        <h1 className="cv-name">{generalInfo.name || "Your Name"}</h1>
+        <div className="cv-contact-info">
+          {generalInfo.email && (
+            <div className="info-item">
+              <img
+                className="icon"
+                src={emailIcon}
+              />
+              <span className="contact-item">{generalInfo.email}</span>
+            </div>
+          )}
+          {generalInfo.phone && (
+            <div className="info-item">
+              <img
+                className="icon"
+                src={phoneIcon}
+              />
+              <span className="contact-item">{generalInfo.phone}</span>
+            </div>
+          )}
+          {generalInfo.linkedin && (
+            <div className="info-item">
+              <img
+                className="icon"
+                src={linkedinIcon}
+              />
+              <a
+                href={normalizeUrl(generalInfo.linkedin)}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="contact-item"
+              >
+                {getUsername(generalInfo.linkedin)}
+              </a>
+            </div>
+          )}
 
-        {generalInfo.github && (
-          <div className="info-item">
-            <img
-              className="icon"
-              src={githubIcon}
-            />
-            <a
-              href={normalizeUrl(generalInfo.github)}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="contact-item"
-            >
-              {getUsername(generalInfo.github)}
-            </a>
-          </div>
-        )}
+          {generalInfo.github && (
+            <div className="info-item">
+              <img
+                className="icon"
+                src={githubIcon}
+              />
+              <a
+                href={normalizeUrl(generalInfo.github)}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="contact-item"
+              >
+                {getUsername(generalInfo.github)}
+              </a>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+      <button
+        className="print-btn"
+        onClick={() => window.print()}
+      >
+        Save as PDF/Print
+      </button>
+    </>
   );
 }
